70b00d60bac42d045bba1b078190cb39a630fe68
Kazushi Kawamura <wakaka2277@gmail.com>
2017-10-08 11:49:37 +0900

    Initial commit :tada:

---
+++ thyme.plugin.zsh
@@ -0,0 +1,11 @@
+PROMPT='%{$fg[blue]%}~%C%{$reset_color%}'
+
+autoload -Uz vcs_info
+setopt prompt_subst
+zstyle ':vcs_info:git:*' check-for-changes true
+zstyle ':vcs_info:git:*' stagedstr "%F{yellow}!"
+zstyle ':vcs_info:git:*' unstagedstr "%F{red}+"
+zstyle ':vcs_info:*' formats "%F{green}%c%u(%b)%f"
+zstyle ':vcs_info:*' actionformats '(%b|%a)'
+precmd () { vcs_info }
+PROMPT=$PROMPT'${vcs_info_msg_0_} $ '
