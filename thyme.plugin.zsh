8bc243d031a54d3e388ccb94dfb33815aee1fa15
Kazushi Kawamura <wakaka2277@gmail.com>
2021-06-10 11:33:15 +0900

    simplify

--- thyme.plugin.zsh
+++ thyme.plugin.zsh
@@ -1,10 +1,10 @@
-PROMPT='%{$fg[blue]%}~%C%{$reset_color%}'
+PROMPT='%{$fg[blue]%}%C%{$reset_color%}'
 
 autoload -Uz vcs_info
 setopt prompt_subst
 zstyle ':vcs_info:git:*' check-for-changes true
-zstyle ':vcs_info:git:*' stagedstr "%F{yellow}!"
-zstyle ':vcs_info:git:*' unstagedstr "%F{red}+"
+zstyle ':vcs_info:git:*' stagedstr "%F{yellow}"
+zstyle ':vcs_info:git:*' unstagedstr "%F{red}"
 zstyle ':vcs_info:*' formats "%F{green}%c%u(%b)%f"
 zstyle ':vcs_info:*' actionformats '(%b|%a)'
 precmd () { vcs_info }
